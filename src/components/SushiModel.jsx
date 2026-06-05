041626e8d2962bca14bfc76229ec7adf8845334a
Justice Entsie <entsie4561@gmail.com>
2026-06-05 16:41:59 +0100

    3d spin

--- src/components/SushiModel.jsx
+++ src/components/SushiModel.jsx
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
 import { useGLTF, Float } from '@react-three/drei';
 
 export default function SushiModel(props) {
   const { scene } = useGLTF('/sushi.glb');
+  const groupRef = useRef();
+
+  // Spin the model slowly on the Y axis every frame
+  useFrame((state, delta) => {
+    if (groupRef.current) {
+      // Negative rotation on Y axis spins it clockwise (when viewed from above)
+      groupRef.current.rotation.y -= delta * 0.3;
+    }
+  });
   
   return (
     <Float 
       speed={2} 
-      rotationIntensity={0.5} 
+      rotationIntensity={0} // Disable the random sway so it only spins smoothly
       floatIntensity={0.5} 
     >
-      {/* We use primitive to render a Three.js scene object */}
-      <primitive object={scene} {...props} />
+      <group ref={groupRef}>
+        {/* We use primitive to render a Three.js scene object */}
+        <primitive object={scene} {...props} />
+      </group>
     </Float>
   );
 }
